4751bfe19929ef7d58f0bee8857ea109a578a85c
Johnata Ferreira <johnatafsantos@gmail.com>
2020-07-28 20:28:34 -0300

    Commit cadastro video e categoria

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+//Desafio master blaster: criar uma página de jogo para a página 404
+const Pagina404 = () => (<div>Página 404 => <Link to="/">Go Home</Link></div>);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} /> 
+    </Switch>
+  </BrowserRouter>,
+  /*<React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>,*/
+  document.getElementById('root') //Container que terá toda nossa aplicação
 );
